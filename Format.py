a9dd74649186ed70ff28713310ec8de511d62ebf
VladimirSteshin <warheim24@gmail.com>
2022-04-15 20:39:56 +0300

    X3, make it tomorrow

--- Format.py
+++ Format.py
@@ -1,6 +1,22 @@
 import json
-
+manager_sales = {}
 with open(r"manager_sales.json", "r", encoding="UTF-8") as file:
     data = json.load(file)
     str_json = json.dumps(data, indent=1)
     work = json.loads(str_json)
+    for content in work:
+        for key, value in content.items():
+            name = ''
+            worklist = 0
+            if key == 'manager':
+                for who in value.values():
+                    name += who + ' '
+            elif key == 'cars':
+                for i in value:
+                    worklist += i['price']
+            manager_sales[name] = worklist
+
+
+print(manager_sales)
+
+
