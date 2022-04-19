64de42c63c859a8d57e1ee34da8f5cda175ce70a
VladimirSteshin <warheim24@gmail.com>
2022-04-19 20:09:11 +0300

    ready to go

--- Format.py
+++ Format.py
@@ -1,22 +1,21 @@
 import json
-manager_sales = {}
+
+managers = {}
 with open(r"manager_sales.json", "r", encoding="UTF-8") as file:
     data = json.load(file)
-    str_json = json.dumps(data, indent=1)
-    work = json.loads(str_json)
-    for content in work:
-        for key, value in content.items():
-            name = ''
-            worklist = 0
-            if key == 'manager':
-                for who in value.values():
-                    name += who + ' '
-            elif key == 'cars':
-                for i in value:
-                    worklist += i['price']
-            manager_sales[name] = worklist
 
+for i in data:
+    person = ''
+    price = 0
+    for name in i['manager'].values():
+        person += name + ' '
+        person.strip()
+    for cost in i['cars']:
+        price += cost['price']
+    managers[person] = price
 
-print(manager_sales)
-
+winner = max(managers.values())
+for key, value in managers.items():
+    if value == winner:
+        print(f'{key}{value}')
 
